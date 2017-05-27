82ace11db2561972e27df4431f6b22868d31fe30
nsw3550 <tenkenx@gmail.com>
2017-05-26 22:51:03 -0700

    Updated parsing logic to be simpler.  Broke down directory generation into a different method.  There's a bug where path.Split thinks the end of the path is a file and not a directory.  Remember to fix.

--- scraper.go
+++ scraper.go
@@ -5,61 +5,26 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"os"
-	"path"
-	"regexp"
-	"strings"
 
 	"golang.org/x/net/html"
 
-	"io"
+	"path"
+	"os"
 )
 
-var root string
-var site string
-var cCase []*regexp.Regexp
-
 // getBody pulls the html body from a given URL.
-func getBody(url string) (io.Reader, error) {
+func getBody(url string) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 
-	return resp.Body, nil
+	return resp, nil
 }
 
-// cornerCase generates regex objects to iterate through.
-func cornerCase() {
-	// Corner cases to ignore
-	badCases := []string{"^#.*", "mailto:.*", ".*pdf"}
-	for _, re := range badCases {
-		cCase = append(cCase, regexp.MustCompile(re))
-	}
-}
-
-// checkCorner checks a string against multiple regex objects.
-func checkCorner(s string) bool {
-	// Iterate through corner case regexes
-	for _, re := range cCase {
-		if re.MatchString(s) {
-			return true
-		}
-	}
-	return false
-}
-
-// parseBody goes through an HTML page and parses all hyperlinks.
-func parseBody(body io.Reader) ([]string, error) {
-	z := html.NewTokenizer(body)
-	results := []string{}
-
-	// Starts with http
-	sReg := regexp.MustCompile("^http.*")
-	// Starts with a /
-	rReg := regexp.MustCompile("^/.*")
-	// Check for files
-	fReg := regexp.MustCompile("^.*\\..*")
+func parseBody(resp *http.Response) ([]*url.URL, error) {
+	z := html.NewTokenizer(resp.Body)
+	results := []*url.URL{}
 
 	for {
 		tt := z.Next()
@@ -68,51 +33,14 @@ func parseBody(body io.Reader) ([]string, error) {
 			return results, nil
 		case tt == html.StartTagToken:
 			t := z.Token()
-
 			if t.Data == "a" {
 				for _, a := range t.Attr {
 					if a.Key == "href" {
-						// Check for corner cases to ignore
-						if checkCorner(a.Val) {
-							break
+						u, err := resp.Request.URL.Parse(a.Val)
+						if err != nil {
+							return nil, err
 						}
-						// Check for absolute links
-						if sReg.MatchString(a.Val) {
-							u, err := url.Parse(a.Val)
-							if err != nil {
-								return nil, fmt.Errorf("could not parse url %v", a.Val)
-							}
-							if strings.HasSuffix(u.Path, "/") {
-								results = append(results, u.Host+u.Path)
-							} else {
-								if !fReg.MatchString(u.Path) {
-									u.Path = u.Path + "/"
-								}
-								results = append(results, u.Host+u.Path)
-							}
-							break
-						}
-						// Check for relative links from root
-						if rReg.MatchString(a.Val) {
-							if strings.HasSuffix(a.Val, "/") {
-								results = append(results, root+a.Val)
-							} else {
-								if !fReg.MatchString(a.Val) {
-									a.Val = a.Val + "/"
-								}
-								results = append(results, root+a.Val)
-							}
-							break
-						}
-						if strings.HasSuffix(a.Val, "/") {
-							results = append(results, site+a.Val)
-						} else {
-							if !fReg.MatchString(a.Val) {
-								a.Val = a.Val + "/"
-							}
-							results = append(results, site+a.Val)
-						}
-						break
+						results = append(results, u)
 					}
 				}
 			}
@@ -120,41 +48,46 @@ func parseBody(body io.Reader) ([]string, error) {
 	}
 }
 
-func main() {
-	cornerCase()
-	s := "http://www.rit.edu/myrit/"
-	tempSite, err := url.Parse(s)
-	if err != nil {
-		log.Fatalf("%v is not a valid url", s)
+func buildDirs(links []*url.URL) []string {
+	result := []string{}
+	//fReg := regexp.MustCompile(".*\\..*")
+	for _, link := range links {
+		if link.Host == "" {
+			continue
+		}
+		if link.Path == "" {
+			result = append(result, link.Host)
+			continue
+		}
+		result = append(result, link.Host+link.Path)
 	}
-	site = tempSite.Host + tempSite.Path
-	data, err := getBody(s)
+
+	return result
+}
+
+func main() {
+	s := "http://www.rit.edu/myrit/"
+	resp, err := getBody(s)
 	if err != nil {
 		log.Fatalf("Could not get HTTP data: %v", err)
 	}
 
-	// Find the root site up to the first / after http(s)
-	r, err := url.Parse(s)
+	links, err := parseBody(resp)
 	if err != nil {
-		log.Fatalf("Invalid site name")
-	}
-	root = r.Host
-
-	urls, err := parseBody(data)
-	if err != nil {
-		log.Fatalf("Could not parse HTTP data: %v", err)
+		log.Fatalf("Could not parse body: %v", err)
 	}
 
-	for i, u := range urls {
+	results := buildDirs(links)
+	for i, r := range results {
 		if i < 5 {
-			d, f := path.Split(u)
+			d, f := path.Split(r)
 			if d == "" {
-				fmt.Printf("No directory for %v\n", u)
+				fmt.Printf("No directory for %v\n", r)
 				continue
 			}
 			os.MkdirAll(d, 0777)
 			if f != "" {
-				newF, err := os.Create(d + f)
+				newF, err := os.Create(d+f)
 				if err != nil {
 					fmt.Println("Could not create file")
 				}
@@ -162,5 +95,4 @@ func main() {
 			}
 		}
 	}
-
 }
